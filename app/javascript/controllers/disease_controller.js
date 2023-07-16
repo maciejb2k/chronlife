0207780b3abf3985d111893b1ee8909dfab2571c
Maciej Biel <maciek.b2k@gmail.com>
2023-07-16 13:06:44 +0200

    Refactor feather icons to remixicons

--- app/javascript/controllers/disease_controller.js
+++ app/javascript/controllers/disease_controller.js
@@ -1,11 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
-import Swal from "sweetalert2";
+import { Toast } from "../utils/toast";
 
 // Connects to data-controller="disease"
 export default class extends Controller {
   connect() {}
 
   confirmDelete(e) {
-    Swal.fire("Any fool can use a computer");
+    Toast.fire({
+      icon: "success",
+      title: "Poprawnie usunięto chorobę",
+    });
   }
 }
---
+++ app/javascript/utils/toast.js
@@ -0,0 +1,13 @@
+import Swal from "sweetalert2";
+
+export const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
