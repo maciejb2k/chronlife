a41c11a883d07bf789c65cc54483ef47f6638303
Maciej Biel <maciek.b2k@gmail.com>
2023-08-11 14:03:00 +0200

    Fix reactions bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,12 @@ class PostsController < BaseController
       .order(created_at: :desc),
       items: 10
     )
+
+    @liked_statuses = Reaction.where(
+      account: current_account,
+      reaction_type: "like",
+      reactable_type: "DiseaseStatus"
+    ).pluck(:reactable_id)
   end
 
   def destroy; end
