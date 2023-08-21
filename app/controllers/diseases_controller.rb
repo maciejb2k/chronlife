30034ee3022b277ddeff37e2ff826fc22c2562bb
Maciej Biel <maciek.b2k@gmail.com>
2023-08-21 14:08:23 +0200

    Refactor diseases controller

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,45 +1,41 @@
 class DiseasesController < BaseController
   before_action :set_disease, only: %i[show edit update destroy]
-  before_action :load_predefined_diseases, only: %i[new edit create]
+  before_action :set_predefined_diseases, only: %i[new create edit update]
   before_action :set_breadcrumbs
 
   def index
     @pagy, @diseases = pagy(
-      current_account.diseases.includes(:predefined_disease).all.order(severity: :desc)
+      current_account
+      .diseases
+      .includes(:predefined_disease)
+      .order(severity: :desc)
     )
-  end
-
-  def show
-    @disease_name = disease_name
   end
 
   def new
     @disease = Disease.new
   end
 
-  def edit
-    @selected_disease_id = @disease.predefined_disease.id
-  end
-
   def create
-    @disease = current_account.diseases.new(disease_params)
+    @disease = current_account.diseases.build(disease_params)
 
     respond_to do |format|
       if @disease.save
-        format.html { redirect_to diseases_path, notice: "Choroba została poprawnie dodana." }
+        format.html { redirect_to diseases_path, notice: t(".success") }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
 
+  def edit
+    @selected_disease_id = @disease.predefined_disease.id
+  end
+
   def update
     respond_to do |format|
       if @disease.update(disease_params)
-        format.html do
-          redirect_to disease_url(@disease),
-                      notice: "Dane choroby zostały poprawnie zaktualizowane."
-        end
+        format.html { redirect_to disease_url(@disease), notice: t(".success") }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -50,8 +46,7 @@ class DiseasesController < BaseController
     @disease.destroy
 
     respond_to do |format|
-      format.html { redirect_to diseases_url, notice: "Choroba została poprawnie usunięta." }
-      format.json { head :no_content }
+      format.html { redirect_to diseases_url, notice: t(".success") }
     end
   end
 
@@ -61,31 +56,27 @@ class DiseasesController < BaseController
     @disease = current_account.diseases.find(params[:id])
   end
 
-  def disease_name
-    @disease.predefined_disease.name.capitalize
-  end
-
   def disease_params
     params.require(:disease).permit(:name, :diagnosed_at, :diagnosed_by_hp, :severity, :color,
                                     :predefined_disease_id)
   end
 
+  def set_predefined_diseases
+    @predefined_diseases = PredefinedDisease.all.map { |d| [d.name, d.id] }
+  end
+
   def set_breadcrumbs
-    add_breadcrumb "home", authenticated_root_path
-    add_breadcrumb "choroby", diseases_path
+    add_breadcrumb t(".breadcrumbs.home"), authenticated_root_path
+    add_breadcrumb t(".breadcrumbs.index"), diseases_path
 
     case action_name.to_sym
     when :new, :create
-      add_breadcrumb "nowa choroba", new_disease_path
+      add_breadcrumb t(".breadcrumbs.new"), new_disease_path
     when :show
-      add_breadcrumb disease_name, @disease
+      add_breadcrumb @disease.predefined_disease.name, @disease
     when :edit, :update
-      add_breadcrumb disease_name, @disease
-      add_breadcrumb "edytuj chorobę", edit_disease_path(@disease)
+      add_breadcrumb @disease.predefined_disease.name, @disease
+      add_breadcrumb t(".breadcrumbs.edit"), edit_disease_path(@disease)
     end
-  end
-
-  def load_predefined_diseases
-    @predefined_diseases = PredefinedDisease.all.map { |d| [d.name.capitalize, d.id] }
   end
 end
