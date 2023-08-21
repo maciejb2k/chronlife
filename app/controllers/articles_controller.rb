3d633201679feea24928b6e24f5db9d2de28796d
Maciej Biel <maciek.b2k@gmail.com>
2023-08-21 17:18:50 +0200

    Refactor articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,20 @@
 class ArticlesController < ApplicationController
-  layout "dashboard"
+  include DashboardLayout
 
   before_action :set_article, only: %i[show edit update destroy]
-
   before_action :set_breadcrumbs
-  before_action :set_breadcrumbs_new, only: %i[new create]
-  before_action :set_breadcrumbs_show, only: %i[show]
-  before_action :set_breadcrumbs_edit, only: %i[edit update]
 
   def index
-    @pagy, @articles = pagy(Article.order(updated_at: :desc))
+    @pagy, @articles = pagy(
+      Article
+      .includes(:account)
+      .order(updated_at: :desc)
+    )
   end
-
-  def show; end
 
   def new
     @article = Article.new
   end
-
-  def edit; end
 
   def create
     @article = Article.new(article_params)
@@ -26,9 +22,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html do
-          redirect_to article_url(@article), notice: "Artykuł został pomyślnie utworzony i opublikowany."
-        end
+        format.html { redirect_to article_url(@article), notice: t(".success") }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -38,9 +32,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html do
-          redirect_to article_url(@article), notice: "Artykuł został pomyślnie zaktualizowany."
-        end
+        format.html { redirect_to article_url(@article), notice: t(".success") }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -51,7 +43,7 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Artykuł został pomyślnie usunięty." }
+      format.html { redirect_to articles_url, notice: t(".success") }
     end
   end
 
@@ -66,19 +58,17 @@ class ArticlesController < ApplicationController
   end
 
   def set_breadcrumbs
-    add_breadcrumb("home", authenticated_root_path)
-    add_breadcrumb("strefa wiedzy", articles_path)
-  end
+    add_breadcrumb t("breadcrumbs.home"), authenticated_root_path
+    add_breadcrumb t(".breadcrumbs.index"), articles_path
 
-  def set_breadcrumbs_new
-    add_breadcrumb("nowy artykuł", new_article_path)
-  end
-
-  def set_breadcrumbs_show
-    add_breadcrumb(@article.title, @article)
-  end
-
-  def set_breadcrumbs_edit
-    add_breadcrumb("edytuj artykuł", edit_article_path(@article))
+    case action_name.to_sym
+    when :new, :create
+      add_breadcrumb t(".breadcrumbs.new"), new_article_path
+    when :show
+      add_breadcrumb @article.title, @article
+    when :edit, :update
+      add_breadcrumb @article.title, @article
+      add_breadcrumb t(".breadcrumbs.edit"), edit_article_path(@article)
+    end
   end
 end
