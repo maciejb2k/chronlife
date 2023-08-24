e59afd5665f88784444bd2898c5ca487bb064dbe
Maciej Biel <maciek.b2k@gmail.com>
2023-08-24 14:48:12 +0200

    Add disease risk factor specs

--- spec/models/disease_risk_factor_spec.rb
+++ spec/models/disease_risk_factor_spec.rb
@@ -18,8 +18,39 @@
 #
 #  fk_rails_...  (disease_id => diseases.id)
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe DiseaseRiskFactor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Factory" do
+    it "has valid factory" do
+      expect(create(:disease_risk_factor)).to be_valid
+    end
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:disease).inverse_of(:risk_factors) }
+  end
+
+  describe "validations" do
+    describe "name" do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_length_of(:name).is_at_most(100) }
+    end
+
+    describe "description" do
+      it { is_expected.to validate_presence_of(:description) }
+      it { is_expected.to validate_length_of(:description).is_at_most(500) }
+    end
+
+    describe "severity" do
+      it { is_expected.to validate_presence_of(:severity) }
+
+      it do
+        is_expected.to validate_numericality_of(:severity)
+          .only_integer
+          .is_greater_than_or_equal_to(1)
+          .is_less_than_or_equal_to(5)
+      end
+    end
+  end
 end
