1b2c9a1d82fed2292a6b087a05b5999240839bd0
Maciej Biel <maciek.b2k@gmail.com>
2023-08-25 14:02:00 +0200

    Add roles specs

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -13,6 +13,6 @@
 #
 FactoryBot.define do
   factory :role do
-    
+    name { Faker::Name.unique.name }
   end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -11,8 +11,23 @@
 #
 #  index_roles_on_name  (name) UNIQUE
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "factory" do
+    it { expect(build(:role)).to be_valid }
+  end
+
+  describe "associations" do
+    it { is_expected.to have_many(:user_roles).dependent(:destroy) }
+    it { is_expected.to have_many(:users).through(:user_roles) }
+  end
+
+  describe "validations" do
+    describe "name" do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_uniqueness_of(:name) }
+      it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    end
+  end
 end
