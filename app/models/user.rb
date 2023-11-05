71159e2ce289176645cbef8b1ba4c844e164ba0f
Maciej Biel <maciek.b2k@gmail.com>
2023-11-05 17:43:46 +0100

    Fix omniauth bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -103,6 +103,7 @@ class User < ApplicationRecord
   def self.from_omniauth!(auth)
     find_or_create_by!(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
+      user.tos_agreement = true
       user.password = Devise.friendly_token[0, 20]
       user.skip_confirmation!
     end
